81b6be95df6c582d18ed396086fc09176a27d995
Tony Lai <a.lai@berkeley.edu>
2016-01-30 20:07:09 -0800

    MORE STUFF WORKS AYYY

--- furNewYearResolutioners/app/src/main/java/com/example/sturmgewehr44/frnewyearresolutioners/MainActivity.java
+++ furNewYearResolutioners/app/src/main/java/com/example/sturmgewehr44/frnewyearresolutioners/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sturmgewehr44.frnewyearresolutioners;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        currentExercise = Exercise.PUSHUP;
+        tagMapToExercise = new HashMap<String, Exercise>();
+        addValuestoHashmap();
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -30,7 +36,38 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeExercise(View view) {
         System.out.println(view.getTag().toString());
+        currentExercise = tagMapToExercise.get(view.getTag().toString());
+        view.setBackgroundColor(Color.YELLOW);
+        TextView ending = (TextView) findViewById(R.id.ending);
+        System.out.println(ending.getText());
+        ending.setText("R.strings");
+        System.out.println(ending.getText());
     }
+
+    private enum Exercise {
+        PUSHUP, SITUP, SQUATS, LEGLIFT, JUMPINGJACKS, PULLUP,
+        CYCLING, WALKING, JOGGING, SWIMMING, STAIRCLIMBING
+    }
+
+    private void addValuestoHashmap() {
+        tagMapToExercise.put("PUSHUP", Exercise.PUSHUP);
+        tagMapToExercise.put("SITUP", Exercise.SITUP);
+        tagMapToExercise.put("SQUATS", Exercise.SQUATS);
+        tagMapToExercise.put("LEGLIFT", Exercise.LEGLIFT);
+        tagMapToExercise.put("JUMPINGJACKS", Exercise.JUMPINGJACKS);
+        tagMapToExercise.put("PULLUP", Exercise.PULLUP);
+        tagMapToExercise.put("CYCLING", Exercise.CYCLING);
+        tagMapToExercise.put("WALKING", Exercise.WALKING);
+        tagMapToExercise.put("JOGGING", Exercise.JOGGING);
+        tagMapToExercise.put("SWIMMING", Exercise.SWIMMING);
+        tagMapToExercise.put("STAIRCLIMBING", Exercise.STAIRCLIMBING);
+    }
+
+    private Exercise currentExercise;
+
+    public int Calories;
+
+    private HashMap<String, Exercise> tagMapToExercise;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
